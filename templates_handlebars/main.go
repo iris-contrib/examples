34c24c79a582190331b9165f31f0cfde9f3d6224
Makis Maropoulos <makis@ideopod.com>
2016-06-28 17:58:37 +0300

    add full support for handlbars with yield and return

--- templates_handlebars/main.go
+++ templates_handlebars/main.go
@@ -11,22 +11,42 @@ import (
 func main() {
 	// set the template engine
 	iris.Config.Render.Template.Engine = iris.HandlebarsEngine
+	iris.Config.Render.Template.Layout = "layouts/layout.html" // default ""
+
+	// NOTE:
+	// the Iris' route framework {{url "my-routename" myparams}} and {{urlpath "my-routename" myparams}} are working like all other template engines,
+	// so  avoid custom url and urlpath helpers.
 
 	// optionaly set handlebars helpers by importing "github.com/aymerick/raymond" when you need to return and render html
 	iris.Config.Render.Template.Handlebars.Helpers["boldme"] = func(input string) raymond.SafeString {
 		return raymond.SafeString("<b> " + input + "</b>")
 	}
 
-	// NOTE:
-	// the Iris' route framework {{url "my-routename" myparams}} and {{urlpath "my-routename" myparams}} are working like all other template engines,
-	// so  avoid custom url and urlpath helpers.
-
 	iris.Get("/", func(ctx *iris.Context) {
 		// optionally, set a context  for the template
-		mycontext := iris.Map{"Name": "Iris", "Type": "Web"}
-
-		ctx.Render("home.html", mycontext)
+		if err := ctx.Render("home.html", map[string]interface{}{"Name": "Iris", "Type": "Web", "Path": "/"}); err != nil {
+			println(err.Error())
+		}
 	})
+
+	// remove the layout for a specific route
+	iris.Get("/nolayout", func(ctx *iris.Context) {
+		if err := ctx.Render("home.html", nil, iris.NoLayout); err != nil {
+			println(err.Error())
+		}
+	})
+
+	// set a layout for a party, .Layout should be BEFORE any Get or other Handle party's method
+	my := iris.Party("/my").Layout("layouts/mylayout.html")
+	{
+		my.Get("/", func(ctx *iris.Context) {
+			ctx.MustRender("home.html", map[string]interface{}{"Name": "Iris", "Type": "Web", "Path": "/my/"})
+		})
+		my.Get("/other", func(ctx *iris.Context) {
+			ctx.MustRender("home.html", map[string]interface{}{"Name": "Iris", "Type": "Web", "Path": "/my/other"})
+		})
+	}
+
 	iris.Listen(":8080")
 }
 
