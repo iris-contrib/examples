047694afba3973e0be6731397fb7cae930bc86af
Makis Maropoulos <makis@ideopod.com>
2016-06-30 06:06:46 +0300

    Add a simple example (and test) for https://github.com/kataras/iris/issues/231

---
+++ sessions_subdomains/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"github.com/kataras/iris"
+)
+
+func main() {
+	domain := "localhost.com"
+
+	// TO DISABLE SUBDOMAIN PERSISTANCE USE THAT:
+	// iris.Config.Sessions.DisableSubdomainPersistance = true
+
+	// set the subdomain from anywhere, here we do from domain
+	iris.Get("/", func(ctx *iris.Context) {
+		//set the session
+		myusername := "iris"
+		ctx.Session().Set("username", myusername)
+		ctx.Write("Username setted to %s, go to /user or mysubdomain.%s:8080/user to view the session's username's value", myusername, domain)
+	})
+
+	iris.Get("/user", func(ctx *iris.Context) {
+		// get the session
+		myusername := ctx.Session().GetString("username")
+		ctx.Write("Hello, your username is %s", myusername)
+	})
+
+	subdomain := iris.Party("mysubdomain.")
+	{
+		// set the session from anywhere either from subdomain either from the domain, here we do from subdomain
+		subdomain.Get("/", func(ctx *iris.Context) {
+			//set the session
+			myusername := "iris"
+			ctx.Session().Set("username", myusername)
+			ctx.Write("Username setted FROM THE SUBDOMAIN to %s, go to /user or mysubdomain.%s/user to view the session's username's value", myusername, domain)
+		})
+
+		subdomain.Get("/user", func(ctx *iris.Context) {
+			// get the session
+			myusername := ctx.Session().GetString("username")
+			ctx.Write("Hello from subdomain, your username is %s", myusername)
+		})
+	}
+
+	iris.Listen(domain + ":8080")
+}
