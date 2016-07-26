44133ca62a57b6a30a2d0cf122d7a45362de2cc7
Gerasimos Maropoulos <makis@ideopod.com>
2016-07-26 18:37:35 +0300

    example for binary data templates

---
+++ template_engines/template_binary/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"github.com/iris-contrib/template/html"
+	"github.com/kataras/iris"
+)
+
+func main() {
+	iris.Config.IsDevelopment = true // this will reload the templates on each request, defaults to false
+
+	//$ go-bindata templates/...
+	// templates are not used, you can delete the folder and run the example
+	iris.UseTemplate(html.New()).Directory("./templates", ".html").Binary(Asset, AssetNames)
+
+	iris.Get("/hi", hi)
+	iris.Listen(":8080")
+}
+
+func hi(ctx *iris.Context) {
+	ctx.MustRender("hi.html", struct{ Name string }{Name: "iris"})
+}
