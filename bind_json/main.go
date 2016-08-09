b3cce6996a08abd45c05acff1e3a4cfb254ee127
Gerasimos Maropoulos <makis@ideopod.com>
2016-08-09 21:29:03 +0300

    Add example for Layout on Pug/Jade

---
+++ bind_json/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"github.com/kataras/iris"
+)
+
+type Company struct {
+	Name string
+	City string
+}
+
+func MyHandler(ctx *iris.Context) {
+	c := &Company{}
+	if err := ctx.ReadJSON(c); err != nil {
+		panic(err.Error())
+	} else {
+		ctx.Write("Company: %#v", c)
+	}
+}
+
+func main() {
+	// use postman or whatever to do a POST request to the localhost:8080/bind_json with BODY: JSON PAYLOAD and HEADERS content type to application/json
+	iris.Post("/bind_json", MyHandler)
+	iris.Listen(":8080")
+}
