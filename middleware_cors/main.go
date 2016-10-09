695706d7628409e9efb024b1902b640c45f82bc9
Gerasimos Maropoulos <kataras2006@hotmail.com>
2016-10-09 21:09:17 +0300

    Add cors middleware simplest example

---
+++ middleware_cors/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"github.com/iris-contrib/middleware/cors"
+	"github.com/kataras/iris"
+)
+
+func main() {
+	app := iris.New()
+	app.Use(cors.Default())
+
+	app.Post("/user/create", func(ctx *iris.Context) {
+		ctx.Write("POST CREATE")
+	}) //db.insert...}
+	app.Put("/user/edit/:email", func(ctx *iris.Context) {
+		ctx.Write("PUT EDIT EMAIL: %s", ctx.Param("email"))
+	}) //db.update...}
+
+	app.Listen(":3333")
+	//	or
+	//	ln, err := iris.TCP4("localhost:3333")
+	//	if err != nil {
+	//		panic(err)
+	//	}
+
+	//	app.Serve(ln)
+}
