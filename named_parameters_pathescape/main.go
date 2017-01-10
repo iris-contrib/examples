05a49eb1698711a81474c0b7e689514e24251c16
Gerasimos (Makis) Maropoulos <kataras2006@hotmail.com>
2017-01-10 09:13:25 +0200

    Add a pathescape example

--- named_parameters_pathescape/main.go
+++ named_parameters_pathescape/main.go
@@ -3,9 +3,18 @@ package main
 import "github.com/kataras/iris"
 
 func main() {
-	iris.Set(iris.OptionEnablePathEscape(true))
-	// accepts parameters with slash '/'
-	// Request: http://localhost:8080/details/Project%2FDelta
+
+	// NOTE:
+	// if iris.Config.EnablePathEscape = true and
+	// Request: http://localhost:8080/details/Project%2FDelta then
+	// it will pass the request
+	// as http://localhost:8080/project/details/Project/Delta
+	// and it will response as NOT FOUND
+	// because iris.Config.EnablePathEscape decodes to query the whole path
+	// before parsing the path parameters.
+
+	// accepts %2F as slash '/' wih ParamDecoded
+	// Request: http://localhost:8080/details/Project%2FDelta then
 	// ctx.Param("project") returns the raw named parameter: Project%2FDelta
 	// ctx.ParamDecoded("project") returns Project/Delta
 	iris.Get("/details/:project", func(ctx *iris.Context) {
---
+++ pathescape/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"github.com/kataras/iris"
+)
+
+type MyTemplateData struct {
+	Values iris.Map // just a shortcat of map[string]interface{}
+	Name   string
+}
+
+func main() {
+	// enable path escape in order
+	// to accept something like http://localhost:8080/profile/my%20name
+	// as http://localhost:8080/profile/my name
+	// if it's false (as defaulted) and you want to escape a parameter name you can just use
+	// ctx.ParamDecoded instead of ctx.Param, see 'named_parameters_pathescape' example
+	iris.Config.EnablePathEscape = true
+
+	// http://localhost:8080/profile/yourname
+	iris.Get("/profile/:username", func(ctx *iris.Context) {
+		name := ctx.Param("username")
+
+		ctx.Writef(name)
+	})
+
+	iris.Listen(":8080")
+}
