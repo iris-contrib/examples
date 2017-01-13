9116afad6553b72357f5e29098ab870260f40929
Gerasimos (Makis) Maropoulos <kataras2006@hotmail.com>
2017-01-13 05:58:14 +0200

    Add a better example for i18n. https://github.com/kataras/iris/commit/02802f281e5840eac75442f6fb21caccff011264

--- middleware_internationalization_i18n/main.go
+++ middleware_internationalization_i18n/main.go
@@ -7,19 +7,41 @@ import (
 
 func main() {
 
-	iris.Use(i18n.New(i18n.Config{Default: "en-US",
+	iris.Use(i18n.New(i18n.Config{
+		Default:      "en-US",
+		URLParameter: "lang",
 		Languages: map[string]string{
 			"en-US": "./locales/locale_en-US.ini",
 			"el-GR": "./locales/locale_el-GR.ini",
 			"zh-CN": "./locales/locale_zh-CN.ini"}}))
 
 	iris.Get("/", func(ctx *iris.Context) {
-		hi := ctx.GetFmt("translate")("hi", "maki") // hi is the key, 'maki' is the %s, the second parameter is optional
-		language := ctx.Get("language")             // language is the language key, example 'en-US'
 
+		// it tries to find the language by:
+		// ctx.Get("language") , that should be setted on other middleware before the i18n middleware*
+		// if that was empty then
+		// it tries to find from the URLParameter setted on the configuration
+		// if not found then
+		// it tries to find the language by the "lang" cookie
+		// if didn't found then it it set to the Default setted on the configuration
+
+		// hi is the key, 'kataras' is the %s on the .ini file
+		// the second parameter is optional
+
+		// hi := ctx.Translate("hi", "kataras")
+		// or:
+		hi := i18n.Translate(ctx, "hi", "kataras")
+
+		language := ctx.Get(iris.TranslateLanguageContextKey) // language is the language key, example 'en-US'
+
+		// The first succeed language found saved at the cookie with name ("language"),
+		//  you can change that by changing the value of the:  iris.TranslateLanguageContextKey
 		ctx.Writef("From the language %s translated output: %s", language, hi)
 	})
 
+	// go to http://localhost:8080/?lang=el-GR
+	// or http://localhost:8080
+	// or http://localhost:8080/?lang=zh-CN
 	iris.Listen(":8080")
 
 }
