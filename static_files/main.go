c7a893b976b4b8a17f378bccff03473a31f79b33
Gerasimos (Makis) Maropoulos <kataras2006@hotmail.com>
2017-03-14 01:41:14 +0200

    Update static files example for v6

--- static_files/main.go
+++ static_files/main.go
@@ -1,8 +1,9 @@
 package main
 
 import (
-	"github.com/kataras/go-template/html"
 	"gopkg.in/kataras/iris.v6"
+	"gopkg.in/kataras/iris.v6/adaptors/httprouter"
+	"gopkg.in/kataras/iris.v6/adaptors/view"
 )
 
 type page struct {
@@ -10,18 +11,25 @@ type page struct {
 }
 
 func main() {
-	iris.UseTemplate(html.New()).Directory("./templates/web/default", ".html")
-	iris.OnError(iris.StatusForbidden, func(ctx *iris.Context) {
+
+	app := iris.New()
+	app.Adapt(
+		iris.DevLogger(),
+		httprouter.New(),
+		view.HTML("./templates", ".html"),
+	)
+
+	app.OnError(iris.StatusForbidden, func(ctx *iris.Context) {
 		ctx.HTML(iris.StatusForbidden, "<h1> You are not allowed here </h1>")
 	})
 	// http://localhost:8080/css/bootstrap.min.css
-	iris.StaticWeb("/css", "./resources/css")
+	app.StaticWeb("/css", "./resources/css")
 	// http://localhost:8080/js/jquery-2.1.1.js
-	iris.StaticWeb("/js", "./resources/js")
+	app.StaticWeb("/js", "./resources/js")
 
-	iris.Get("/", func(ctx *iris.Context) {
+	app.Get("/", func(ctx *iris.Context) {
 		ctx.MustRender("something.html", page{Title: "Home"})
 	})
 
-	iris.Listen(":8080")
+	app.Listen("localhost:8080")
 }
