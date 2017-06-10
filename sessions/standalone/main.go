d93e3fa47ee318bdf4dfcc7860762fde88b4daf6
kataras <kataras2006@hotmail.com>
2017-06-10 04:18:11 +0300

    fix some examples

--- sessions/standalone/main.go
+++ sessions/standalone/main.go
@@ -8,6 +8,10 @@ import (
 	"github.com/kataras/iris/sessions"
 )
 
+type businessModel struct {
+	Name string
+}
+
 func main() {
 	app := iris.New()
 	// enable all (error) logs
@@ -29,11 +33,6 @@ func main() {
 		// want to be crazy safe? Take a look at the "securecookie" example folder.
 	})
 
-	// OPTIONALLY:
-	// import "github.com/kataras/iris/sessions/sessiondb/redis"
-	// or import "github.com/kataras/go-sessions/sessiondb/$any_available_community_database"
-	// mySessions.UseDatabase(redis.New(...))
-
 	app.AttachSessionManager(mySessions) // Attach the session manager we just created.
 
 	app.Get("/", func(ctx context.Context) {
@@ -41,15 +40,24 @@ func main() {
 	})
 	app.Get("/set", func(ctx context.Context) {
 
-		//set session values
+		//set session values.
+
 		ctx.Session().Set("name", "iris")
 
 		//test if setted here
 		ctx.Writef("All ok session setted to: %s", ctx.Session().GetString("name"))
+
+		// Set will set the value as-it-is,
+		// if it's a slice or map
+		// you will be able to change it on .Get directly!
+		// Keep note that I don't recommend saving big data neither slices or maps on a session
+		// but if you really need it then use the `SetImmutable` instead of `Set`.
+		// Use `SetImmutable` consistently, it's slower.
+		// Read more about muttable and immutable go types: https://stackoverflow.com/a/8021081
 	})
 
 	app.Get("/get", func(ctx context.Context) {
-		// get a specific key, as string, if no found returns just an empty string
+		// get a specific value, as string, if no found returns just an empty string
 		name := ctx.Session().GetString("name")
 
 		ctx.Writef("The name on the /set was: %s", name)
@@ -75,6 +83,41 @@ func main() {
 	// You can destroy a session outside of a handler too, using the:
 	// mySessions.DestroyByID
 	// mySessions.DestroyAll
+
+	// remember: slices and maps are muttable by-design
+	// The `SetImmutable` makes sure that they will be stored and received
+	// as immutable, so you can't change them directly by mistake.
+	//
+	// Use `SetImmutable` consistently, it's slower than `Set`.
+	// Read more about muttable and immutable go types: https://stackoverflow.com/a/8021081
+	app.Get("/set_immutable", func(ctx context.Context) {
+		business := []businessModel{{Name: "Edward"}, {Name: "value 2"}}
+		ctx.Session().SetImmutable("businessEdit", business)
+		businessGet := ctx.Session().Get("businessEdit").([]businessModel)
+
+		// try to change it, if we used `Set` instead of `SetImmutable` this
+		// change will affect the underline array of the session's value "businessEdit", but now it will not.
+		businessGet[0].Name = "Gabriel"
+
+	})
+
+	app.Get("/get_immutable", func(ctx context.Context) {
+		valSlice := ctx.Session().Get("businessEdit")
+		if valSlice == nil {
+			ctx.HTML("please navigate to the <a href='/set_immutable'>/set_immutable</a> first")
+			return
+		}
+
+		firstModel := valSlice.([]businessModel)[0]
+		// businessGet[0].Name is equal to Edward initially
+		if firstModel.Name != "Edward" {
+			panic("Report this as a bug, immutable data cannot be changed from the caller without re-SetImmutable")
+		}
+
+		ctx.Writef("[]businessModel[0].Name remains: %s", firstModel.Name)
+
+		// the name should remains "Edward"
+	})
 
 	app.Run(iris.Addr(":8080"))
 }
