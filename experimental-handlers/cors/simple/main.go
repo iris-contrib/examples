66f275b4de0b7373de4c31ac786c98fb8cca9470
ZaniaDevelopper <devteam@zania.group>
2018-02-21 14:46:44 +0300

    Modifications after fixes for CORS middleware

--- experimental-handlers/cors/simple/main.go
+++ experimental-handlers/cors/simple/main.go
@@ -5,6 +5,7 @@ package main
 
 import (
 	"github.com/kataras/iris"
+	"github.com/kataras/iris/core/router"
 
 	"github.com/iris-contrib/middleware/cors"
 )
@@ -12,13 +13,15 @@ import (
 func main() {
 
 	app := iris.New()
-	crs := cors.New(cors.Options{
+	crs := cors.NewPartyMiddleware(cors.Options{
 		AllowedOrigins:   []string{"*"}, // allows everything, use that to change the hosts.
 		AllowCredentials: true,
 	})
 
+	var p router.Party
+
 	v1 := app.Party("/api/v1")
-	v1.Use(crs)
+	v1.ConfigureParty(crs)
 	{
 		v1.Get("/home", func(ctx iris.Context) {
 			ctx.WriteString("Hello from /home")
